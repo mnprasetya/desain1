bda19d1e8c29bf9f6037f30642920e80929353d4
Median Prasetya <mnprasetya@gmail.com>
2021-06-14 23:23:45 +0700

    bentar

--- lib/conn.php
+++ lib/conn.php
@@ -1,5 +1,7 @@
 <?php
 error_reporting(0);
+date_default_timezone_set('Asia/Jakarta');
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 $koneksi = mysqli_connect('localhost', 'user', 'password', 'database');
